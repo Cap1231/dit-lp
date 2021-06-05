22759e846ff2ab093ec2bf3efff4c867b6240550
tsubasa <tsubasa.y1231@gmail.com>
2021-06-05 10:23:48 +0900

    feat: homeページ土台作成

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <Link to="/">Homeへ</Link>
+    </div>
+  )
+}
+export default Header
