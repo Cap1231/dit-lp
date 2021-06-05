71114b4894b6b6c10b11e9382b9419b22437bf42
tsubasa <tsubasa.y1231@gmail.com>
2021-06-05 16:58:18 +0900

    feat: header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import Home from './components/Home'
 import Header from './components/Header'
 import { BrowserRouter, Route } from 'react-router-dom'
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 
 function App() {
   return (
     <BrowserRouter>
+      {/* CSS Reset */}
+      <CssBaseline />
+
+      {/* Header */}
       <Header />
+
+      {/* Main */}
       <Route exact path="/" component={Home} />
+
+      {/* Footer */}
     </BrowserRouter>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,90 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom';
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import './Header.scss'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import Drawer from '@material-ui/core/Drawer'
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const Header = () => {
+  const isPC = useMediaQuery('(min-width:768px)')
+  const [isSideBarOpen, SetIsSideBarOpen] = useState(false)
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    SetIsSideBarOpen(open);
+  }
+
+  // SP用のサイドバー
+  const list = () => (
+    <div
+      className="sidebar"
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <ListItem href="/">
+          <ListItemText primary="会社概要"/>
+        </ListItem>
+        <ListItem href="/">
+          <ListItemText primary="サービス/製品一覧"/>
+        </ListItem>
+        <ListItem href="/">
+          <ListItemText primary="FAQ"/>
+        </ListItem>
+        <ListItem href="/">
+          <ListItemText primary="採用情報"/>
+        </ListItem>
+        <ListItem href="/">
+          <ListItemText primary="お問い合わせ"/>
+        </ListItem>
+      </List>
+      <Divider />
+    </div>
+  )
+
   return (
-    <div className='header'>
-      <Link to="/">Homeへ</Link>
+    <div>
+      <AppBar position="static">
+        <Toolbar className="navbar">
+          <h1 className="navbar-brand">
+            Develop IT
+          </h1>
+          { isPC ? (
+            <Typography variant="h6" className="nav-item">
+              <Link to="/" className="nav-link">会社概要</Link>
+              <Link to="/" className="nav-link">サービス/製品一覧</Link>
+              <Link to="/" className="nav-link">FAQ</Link>
+              <Link to="/" className="nav-link">採用情報</Link>
+              <Link to="/" className="nav-link">お問い合わせ</Link>
+            </Typography>
+          ) : (
+            <React.Fragment key='right'>
+              <IconButton onClick={toggleDrawer(true)}>
+                <MenuIcon />
+              </IconButton>
+              <Drawer
+                anchor='right'
+                open={isSideBarOpen}
+                onClose={toggleDrawer(false)}
+              >
+                {list()}
+              </Drawer>
+            </React.Fragment>
+          )}
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }
